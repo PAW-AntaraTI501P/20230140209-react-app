6d942a8f82ce663c9f60d87d3b322e9d62e8ca89
lululthfh <punyalulu05@gmail.com>
2025-09-06 18:21:07 +0700

    feat: add navigation

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import TodoPage from "./pages/Todo/TodoPage";
 import LoginPage from "./pages/Login/LoginPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LandingPage from "./pages/Landing/LandingPage";
+import RegisterPage from "./pages/Register/RegisterPage";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/todos" element={<TodoPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/todos" element={<TodoPage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Route>
       </Routes>
     </Router>
   );
