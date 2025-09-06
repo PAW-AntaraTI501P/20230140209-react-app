ab17a8c016465dcf62c35eaf245534306d63def0
lululthfh <punyalulu05@gmail.com>
2025-09-06 18:22:07 +0700

    fix: resolve conflict

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,9 +1,12 @@
-import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = () =>{
+const ProtectedRoute = () => {
+    // Check for token in localStorage
     const token = localStorage.getItem("token");
+
+    // If a token exists, allow access to the requested page
+    // If not, redirect to the login page
     return token ? <Outlet /> : <Navigate to="/login" />;
-}
+};
 
 export default ProtectedRoute;
